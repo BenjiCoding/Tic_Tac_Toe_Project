a7a33abc408d30455218c9297c25c6c7ca46bc01
Benji Coding <137091447+BenjiCoding@users.noreply.github.com>
2023-06-19 16:13:42 -0400

    Adding the logic of the game

--- main.js
+++ main.js
@@ -3,12 +3,34 @@ const choices = ["Rock", "Paper", "Scissors"];
 function computerChoice(){
     let computer = choices[Math.floor(Math.random() * choices.length)];
     console.log(computer);
+    return computer;
 }
 
 function playerChoice(){
     let userChoice = prompt("Rock, Paper, Scissors?");
     console.log(userChoice);
-    computerChoice();
+    return userChoice;
 }
 
-playerChoice();
+function game(player, computer){
+
+    if(player === computer){
+        alert('DRAW!!')
+    } else if(player === "Rock" && computer === "Scissors") {
+        alert('Player WIN!!')
+    } else if(player === "Rock" && computer === "Paper") {
+        alert('Computer WIN!!')
+    } else if(player === "Scissors" && computer === "Rock") {
+        alert('Computer WIN!!')
+    } else if(player === "Scissors" && computer === "Paper") {
+        alert('Player WIN!!')
+    } else if(player === "Paper" && computer === "Scissors") {
+        alert('Computer WIN!!')
+    } else if(player === "Paper" && computer === "Rock") {
+        alert('Player WIN!!')
+    }
+
+}
+
+
+game(playerChoice(), computerChoice());
