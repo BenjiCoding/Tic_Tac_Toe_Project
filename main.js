df0f28e4340d28097b4b29a2292c5b8d6881094b
Benji Coding <137091447+BenjiCoding@users.noreply.github.com>
2023-06-20 16:09:51 -0400

    All the logic is now implemented

--- main.js
+++ main.js
@@ -3,6 +3,13 @@ var userScore = 0;
 var computerScore = 0;
 
 let choiceButton = document.querySelectorAll('.button');
+let playAgain = document.getElementById('play-again');
+let userText = document.getElementById('playerScore');
+let computerText = document.getElementById('computerScore');
+let winner = document.getElementById('winner');
+
+userText.textContent = 'Player : ' + userScore;
+computerText.textContent = 'Computer : ' + computerScore;
 
 choiceButton.forEach(btn => {
     btn.addEventListener('click', e => {
@@ -10,8 +17,33 @@ choiceButton.forEach(btn => {
         console.log('User Choice : ' + userChoice)
         game(playerChoice(userChoice), computerChoice());
         console.log('Clicked! ' + userScore + ' ' + computerScore);
+        userText.textContent = 'Player : ' + userScore;
+        computerText.textContent = 'Computer : ' + computerScore;
+        if(userScore == 5 || computerScore == 5){
+            alert('Game finish');
+            if(userScore == 5){
+                winner.textContent = "PLAYER WIN !!";
+            } else {
+                winner.textContent = "COMPUTER WIN !!";
+            }
+        }
+        if(userScore == 5 || computerScore == 5){
+            playAgain.style.display = 'block';
+        } else {
+            playAgain.style.display = 'none';
+        }
     })
 });
+
+playAgain.addEventListener('click', e => {
+    userScore = 0;
+    computerScore = 0;
+    userText.textContent = 'Player : ' + userScore;
+    computerText.textContent = 'Computer : ' + computerScore;
+    winner.textContent = "";
+    console.log('PLAY AGAIN');
+})
+
 
 function computerChoice(){
     let computer = choices[Math.floor(Math.random() * choices.length)];
@@ -28,7 +60,7 @@ function game(player, computer){
 
 
     if(player === computer){
-        alert('DRAW!!')
+        //alert('DRAW!!')
     } else if(player === "Rock" && computer === "Scissors") {
         //alert('Player WIN!!')
         userScore += 1;
