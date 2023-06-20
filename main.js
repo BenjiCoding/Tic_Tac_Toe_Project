bbf22d8176d28d6d261e619d88785c2ddee3977a
Benji Coding <137091447+BenjiCoding@users.noreply.github.com>
2023-06-19 20:03:13 -0400

    Adding some more JS script logic at score level

--- main.js
+++ main.js
@@ -2,46 +2,51 @@ const choices = ["Rock", "Paper", "Scissors"];
 var userScore = 0;
 var computerScore = 0;
 
+let choiceButton = document.querySelectorAll('.button');
+
+choiceButton.forEach(btn => {
+    btn.addEventListener('click', e => {
+        let userChoice = e.target.textContent;
+        console.log('User Choice : ' + userChoice)
+        game(playerChoice(userChoice), computerChoice());
+        console.log('Clicked! ' + userScore + ' ' + computerScore);
+    })
+});
+
 function computerChoice(){
     let computer = choices[Math.floor(Math.random() * choices.length)];
     console.log(computer);
     return computer;
 }
 
-function playerChoice(){
-    let userChoice = prompt("Rock, Paper, Scissors?");
-    console.log(userChoice);
+function playerChoice(val){
+    let userChoice = val
     return userChoice;
 }
 
-// Put the both functions inside game function
-// Put the game function inside a onClick event
-
 function game(player, computer){
+
 
     if(player === computer){
         alert('DRAW!!')
     } else if(player === "Rock" && computer === "Scissors") {
-        alert('Player WIN!!')
+        //alert('Player WIN!!')
         userScore += 1;
     } else if(player === "Rock" && computer === "Paper") {
-        alert('Computer WIN!!')
+        //alert('Computer WIN!!')
         computerScore += 1;
     } else if(player === "Scissors" && computer === "Rock") {
-        alert('Computer WIN!!')
+        //alert('Computer WIN!!')
         computerScore += 1;
     } else if(player === "Scissors" && computer === "Paper") {
-        alert('Player WIN!!')
+        //alert('Player WIN!!')
         userScore += 1;
     } else if(player === "Paper" && computer === "Scissors") {
-        alert('Computer WIN!!')
+        //alert('Computer WIN!!')
         computerScore += 1;
     } else if(player === "Paper" && computer === "Rock") {
-        alert('Player WIN!!')
+        //alert('Player WIN!!')
         userScore += 1;
     }
 
 }
-
-
-game(playerChoice(), computerChoice());
